5004c557f938f9968043e91bfdfcbe308a9a8a79
Mykyta Pantelei <falkao0101@gmail.com>
2026-08-14 11:18:30 +0200

    Initial Commit

---
+++ refind/refindApp.swift
@@ -0,0 +1,17 @@
+//
+//  refindApp.swift
+//  refind
+//
+//  Created by Mykyta Pantelei on 14.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct refindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
